b313e506eae81f36d760cbfdcc1d1f1b30b19538
vlbras <vlad.bras02@gmail.com>
2023-09-08 19:24:03 +0200

    build: initial commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+  await app.listen(process.env.PORT);
+  Logger.log(`😎 Application is running on port ${process.env.PORT}`, `bootstrap`);
+}
+bootstrap();
